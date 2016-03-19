8b214bd21fbbe49c835f25fed227c951d9509533
beck <beck@eventbrite.com>
2016-03-18 22:15:44 -0500

    separated code into own functions

--- main.js
+++ main.js
@@ -5,25 +5,14 @@
         });
 
         function buildPieChart(data) {
-            var pie = d3.layout.pie()
-                .value(function(d){
-                    return d.count;
-                }),
+            var pie = createPie(),
                 slices = pie(data),
-                arc = d3.svg.arc()
-                    .innerRadius(0)
-                    .outerRadius(100),
+                arc = createArc(),
                 color = d3.scale.category10(),
                 svg = d3.select('svg.pie'),
-                g = svg.append('g')
-                    .attr('transform', 'translate(200, 100)'),
-                dataTotal = d3.sum(data, function(d) {
-                    return d.count;
-                });
-
-                data.forEach(function(d) {
-                    d.percentage = Math.floor((d.count  / dataTotal) * 100);
-                });
+                g = addgElement(svg),
+                dataTotal = getTotal(data),
+                dataWithPercentages = getDataWithPercentage(data, dataTotal);
 
                 g.selectAll('path.slice')
                     .data(slices)
@@ -35,23 +24,58 @@
                                     return color(d.data.product);
                                 });
 
-                svg.append('g')
-                    .attr('class', 'legend')
-                        .selectAll('text')
-                        .data(slices)
-                            .enter()
-                                .append('text')
-                                    .text(function(d) {
-                                        return '. ' + d.data.product + ' - ' + d.data.percentage + '%';
-                                    })
-                                    .attr('fill', function(d) {
-                                        return color(d.data.product);
-                                    })
-                                    .attr('y', function(d, i) {
-                                        return 20 * (i + 1);
-                                    });
-        }
+                addLegend(svg, dataWithPercentages, slices);
+
     }
 
+    function createPie() {
+        return d3.layout.pie()
+            .value(function(d){
+                return d.count;
+            });
+    }
+
+    function getDataWithPercentage(data, dataTotal) {
+        return data.forEach(function(d) {
+            d.percentage = Math.floor((d.count  / dataTotal) * 100);
+        });
+    }
+
+    function createArc() {
+        return d3.svg.arc()
+            .innerRadius(0)
+            .outerRadius(100);
+    }
+
+    function addgElement(svg) {
+        return svg.append('g')
+            .attr('transform', 'translate(200, 100)');
+    }
+
+    function getTotal(data) {
+        return d3.sum(data, function(d) {
+            return d.count;
+        });
+    }
+
+    function addLegend(svg, data, slices) {
+        var color = d3.scale.category10();
+        svg.append('g')
+            .attr('class', 'legend')
+                .selectAll('text')
+                .data(slices)
+                    .enter()
+                        .append('text')
+                            .text(function(d) {
+                                return '. ' + d.data.product + ' - ' + d.data.percentage + '%';
+                            })
+                            .attr('fill', function(d) {
+                                return color(d.data.product);
+                            })
+                            .attr('y', function(d, i) {
+                                return 20 * (i + 1);
+                            });
+                        }
+    }
     getData();
 })();
